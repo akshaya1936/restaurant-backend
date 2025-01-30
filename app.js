d3967b69da65e6f5efb73711faa6f659ecc8efd3
akshaya <akshayavivek1936@gmail.com>
2025-01-30 21:20:46 +0530

    Replaced bcrypt with bcryptjs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const app = express();
